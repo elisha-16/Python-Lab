aa39d188b9f5cd4666b15998c4c901cb03e272bb
Elisha Ghosh <61902244+elisha-16@users.noreply.github.com>
2020-03-22 10:30:20 +0530

    math operation functions in numpy

---
+++ Numpy math operation.py
@@ -0,0 +1,31 @@
+a=np.array([1,2,3,4])
+b=np.array([10,20,30,40])
+print(np.add(9, 5))       #add arguments#
+print(np.add(a,b))
+
+print(np.subtract(a,b))   #subtarct arguments#
+
+print(np.multiply(a,b))       #multiply arguments#
+
+print(np.divide(a,b))      #divide arguments#
+
+print(np.floor_divide(10,3))  #divide argumentd and returns floor value#
+
+print(np.true_divide(20,6))    #return true division of the input#
+
+print(np.remainder(10,3))    #returns the  remainder#
+
+print(np.negative(30))   #numerical negative#
+print(np.positive(20))    #numerical positive#
+
+print(np.gcd(9,45))       #returns greatest common divisor#
+print(np.lcm(4,5))        #returns lowest common multiple#
+
+a=np.array([1,2,3,4)
+print(np.square(a))      #square of input elements#
+print(np.sqrt(a))         #squareroots of input elements#
+
+
+print(np.cbrt(b))      #returns cuberoot of an array#
+
+
